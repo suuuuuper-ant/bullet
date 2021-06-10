623e46457af642b85221760cc87ba7c51cef00d9
ManHyuk <rnanhyuk@gmail.com>
2021-06-11 00:01:23 +0900

    init commit

---
+++ src/main/kotlin/com/digin/bullet/BulletApplication.kt
@@ -0,0 +1,11 @@
+package com.digin.bullet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BulletApplication
+
+fun main(args: Array<String>) {
+    runApplication<BulletApplication>(*args)
+}
---
+++ src/test/kotlin/com/digin/bullet/BulletApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.digin.bullet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BulletApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
