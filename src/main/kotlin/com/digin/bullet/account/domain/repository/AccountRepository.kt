da630ebfc6fea0997c9de7f961d87ce7f47cb4b1
hubert <hubert.bear@kakaomobility.com>
2021-06-20 02:50:53 +0900

    account repository 샘플 코드

---
+++ src/main/kotlin/com/digin/bullet/account/domain/repository/AccountRepository.kt
@@ -0,0 +1,8 @@
+package com.digin.bullet.account.domain.repository
+
+import com.digin.bullet.account.domain.entity.Account
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface AccountRepository : ReactiveCrudRepository<Account, Long>
