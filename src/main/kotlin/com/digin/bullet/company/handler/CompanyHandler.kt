7e49c2087b2be822f051538f0b8d3976144a82ca
ManHyuk <rnanhyuk@gmail.com>
2021-07-15 01:34:30 +0900

    feat: edit either map

--- src/main/kotlin/com/digin/bullet/company/handler/CompanyHandler.kt
+++ src/main/kotlin/com/digin/bullet/company/handler/CompanyHandler.kt
@@ -1,35 +1,43 @@
 package com.digin.bullet.company.handler
 
-import arrow.core.Either
+import arrow.core.*
 import com.digin.bullet.common.model.http.response.ErrorResponse
 import com.digin.bullet.common.model.http.response.SuccessResponse
 import com.digin.bullet.company.model.dto.CompanyDTO
 import com.digin.bullet.company.model.exception.CompanyException
 import com.digin.bullet.company.service.CompanyService
+import mu.KotlinLogging
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import kotlin.math.log
 
 @Component
 class CompanyHandler(
     private val companyService: CompanyService
 ) {
+    private val log = KotlinLogging.logger {}
 
     suspend fun getCompany(serverRequest: ServerRequest): ServerResponse {
         val stockCode = serverRequest.pathVariable("stockCode")
         return when (val result = companyService.getCompanyByStockCode(stockCode)) {
-            is Either.Left -> when(result.value) {
-                CompanyException.NOT_FOUND_COMPANY -> ServerResponse.badRequest().bodyValueAndAwait(ErrorResponse(result = result.value.name))
+            is Either.Left -> ServerResponse
+                .badRequest()
+                .bodyValueAndAwait(ErrorResponse(result = result.getOrHandle { it.name }))
+            is Either.Right -> {
+                val companyDTO = result.map {
+                    CompanyDTO(
+                        id = it.id!!,
+                        stockCode = it.stockCode,
+                        shortName = it.shortName,
+                        imageUrl = it.imageUrl ?: "",
+                        likeCount = it.likeCount,
+                    )
+                }
+                ServerResponse
+                    .ok()
+                    .bodyValueAndAwait(SuccessResponse(result = companyDTO))
             }
-            is Either.Right -> ServerResponse
-                .ok()
-                .bodyValueAndAwait(SuccessResponse(result = CompanyDTO(
-                    id = result.value.id!!,
-                    stockCode = result.value.stockCode,
-                    shortName = result.value.shortName,
-                    imageUrl = result.value.imageUrl ?: "",
-                    likeCount = result.value.likeCount,
-                )))
 
         }
     }
--- src/main/kotlin/com/digin/bullet/company/model/exception/CompanyException.kt
+++ src/main/kotlin/com/digin/bullet/company/model/exception/CompanyException.kt
@@ -1,5 +1,5 @@
 package com.digin.bullet.company.model.exception
 
 enum class CompanyException {
-    NOT_FOUND_COMPANY
+    NOT_FOUND_COMPANY, TEMP
 }
