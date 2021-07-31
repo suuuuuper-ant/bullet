64d6b987a72bc28ccdbaf442da5660f5982fb266
ManHyuk <rnanhyuk@gmail.com>
2021-08-01 02:17:32 +0900

    fix: fix error

--- src/test/kotlin/com/digin/bullet/config/RestDocConfiguration.kt
+++ src/test/kotlin/com/digin/bullet/config/RestDocConfiguration.kt
@@ -5,29 +5,28 @@ import org.springframework.context.annotation.Bean
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsWebTestClientConfigurationCustomizer
 
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer
 import org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint
 import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentationConfigurer
 
 
-@TestConfiguration
+//@TestConfiguration
 class RestDocConfiguration {
 
-    @Bean
-    fun restDocsMockMvcConfigurationCustomizer(): RestDocsMockMvcConfigurationCustomizer? {
-        return RestDocsMockMvcConfigurationCustomizer { configurer: MockMvcRestDocumentationConfigurer ->
-            configurer.operationPreprocessors()
-                .withRequestDefaults(prettyPrint())
-                .withResponseDefaults(prettyPrint())
-        }
-    }
-
-    @Bean
-    fun restDocsWebTestClientConfigurationCustomizer(): RestDocsWebTestClientConfigurationCustomizer? {
-        return RestDocsWebTestClientConfigurationCustomizer { configurer: WebTestClientRestDocumentationConfigurer ->
-            configurer.operationPreprocessors()
-                .withRequestDefaults(prettyPrint())
-                .withResponseDefaults(prettyPrint())
-        }
-    }
+//    @Bean
+//    fun restDocsMockMvcConfigurationCustomizer(): RestDocsMockMvcConfigurationCustomizer? {
+//        return RestDocsMockMvcConfigurationCustomizer { configurer: MockMvcRestDocumentationConfigurer ->
+//            configurer.operationPreprocessors()
+//                .withRequestDefaults(prettyPrint())
+//                .withResponseDefaults(prettyPrint())
+//        }
+//    }
+//
+//    @Bean
+//    fun restDocsWebTestClientConfigurationCustomizer(): RestDocsWebTestClientConfigurationCustomizer? {
+//        return RestDocsWebTestClientConfigurationCustomizer { configurer: WebTestClientRestDocumentationConfigurer ->
+//            configurer.operationPreprocessors()
+//                .withRequestDefaults(prettyPrint())
+//                .withResponseDefaults(prettyPrint())
+//        }
+//    }
 }
