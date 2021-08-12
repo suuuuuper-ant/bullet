3ce7a4fa03c09566c6c0df85792e2eebb375e41a
ManHyuk <rnanhyuk@gmail.com>
2021-08-13 00:53:31 +0900

    chore: 안쓰는 코드 정리

--- src/main/kotlin/com/digin/bullet/BulletApplication.kt
+++ src/main/kotlin/com/digin/bullet/BulletApplication.kt
@@ -1,22 +1,10 @@
 package com.digin.bullet
 
-import com.digin.bullet.account.repository.AccountRepository
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Bean
-import org.springframework.kotlin.coroutine.EnableCoroutine
 
 @SpringBootApplication
 class BulletApplication {
-
-//    @Bean
-//    fun run(accountRepository: AccountRepository): ApplicationRunner {
-//        return ApplicationRunner {
-//            accountRepository.findAll().subscribe { println(it) }
-//        }
-//    }
 }
 
 fun main(args: Array<String>) {
